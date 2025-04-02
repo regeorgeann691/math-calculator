f4fd8c281b3456e93dfa6b33cc420096258fec01
regeorgeann691 <regeorgeann691@gmail.com>
2025-04-02 01:31:33 +0000

    Add 24.go

---
+++ src/24.go
@@ -0,0 +1,9 @@
+public class MathCalculator {
+
+    public static void main(String[] args) {
+        double a = 3.5;
+        double b = 2.1;
+
+        System.out.println("The sum of " + a + " and " + b + " is: " + (a + b));
+    }
+}
